a86db2880e72ededd1c2473a0d39257563038b7b
omarjafor <omarjaforchy@gmail.com>
2023-09-23 23:43:51 +0600

    basic setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">
-        Hello world! <br></br>
-        Phone Collection React Router
+        Hello world!
       </h1>
     </>
   )
