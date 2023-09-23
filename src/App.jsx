692c2b8b1be9d08f992577e70b7e8f9afa108e25
omarjafor <omarjaforchy@gmail.com>
2023-09-23 23:43:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Hello world! <br></br>
+        Phone Collection React Router by OMAR JAFOR
+      </h1>
+    </>
+  )
+}
+
+export default App
