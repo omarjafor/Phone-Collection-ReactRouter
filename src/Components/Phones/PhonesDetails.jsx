4a11d83278dad215cf369b267742c0456dc1b39d
omarjafor <omarjaforchy@gmail.com>
2023-09-28 00:00:52 +0600

    react router practice

--- src/Components/Phones/PhonesDetails.jsx
+++ src/Components/Phones/PhonesDetails.jsx
@@ -6,7 +6,7 @@ const PhonesDetails = () => {
     const { brand_name, image, phone_name, price, rating } = phone || {};
 
     return (
-        <div>
+        <div className="flex justify-center">
             <div className="relative mt-20 flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
                 <div className="relative mx-4 -mt-6 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
                     <img
