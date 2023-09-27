d37bbcb708d72b518fde7fd45b40d135cc71529f
omarjafor <omarjaforchy@gmail.com>
2023-09-28 00:09:21 +0600

    react router practice

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,7 +1,7 @@
 const Favorites = () => {
     return (
         <div>
-            <h2>Helloooooooo From favorites</h2>
+            <h2 className="text-2xl font-bold h-[90vh] items-center flex justify-center">No Phones Added Till Now</h2>
         </div>
     );
 };
