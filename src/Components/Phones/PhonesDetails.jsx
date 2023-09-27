68c62ca6d7e70e0c12d90512e593dfd6d604d38c
omarjafor <omarjaforchy@gmail.com>
2023-09-27 23:46:11 +0600

    react router practice

--- src/Components/Phones/PhonesDetails.jsx
+++ src/Components/Phones/PhonesDetails.jsx
@@ -3,20 +3,20 @@ import { useLoaderData } from "react-router-dom";
 const PhonesDetails = () => {
 
     const phone = useLoaderData();
-    console.log(phone);
+    const { brand_name, image, phone_name, price, rating } = phone || {};
 
     return (
         <div>
             <div className="relative mt-20 flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-                <div className="relative mx-4 -mt-6 h-56 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+                <div className="relative mx-4 -mt-6 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
                     <img
-                        src=""
+                        src={image}
                         alt="img-blur-shadow"
                     />
                 </div>
                 <div className="p-6">
                     <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                        UI/UX Review Check
+                        {phone_name}
                     </h5>
                     <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
                         The place is close to Barceloneta Beach and bus stop just 2 min by walk
