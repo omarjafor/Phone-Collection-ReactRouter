8b4b292f9dc78038922b876210d3a5cc13689ee9
omarjafor <omarjaforchy@gmail.com>
2023-09-28 06:37:28 +0600

    phone details card

--- src/Components/Phones/PhonesDetails.jsx
+++ src/Components/Phones/PhonesDetails.jsx
@@ -6,30 +6,33 @@ const PhonesDetails = () => {
     const { brand_name, image, phone_name, price, rating } = phone || {};
 
     return (
-        <div className="flex justify-center">
-            <div className="relative mt-20 flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-                <div className="relative mx-4 -mt-6 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+        <div className="flex justify-center mt-4 items-center h-[90vh]">
+            <div className="relative flex w-96 flex-col rounded-xl bg-emerald-400 bg-clip-border text-gray-700 shadow-md">
+                <div className="relative mx-4 mt-2 h-96 overflow-hidden rounded-xl text-gray-700">
                     <img
                         src={image}
-                        alt="img-blur-shadow"
+                        className="h-full w-full object-cover"
                     />
                 </div>
                 <div className="p-6">
-                    <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                        {phone_name}
-                        <p>Price: {price} </p>
-                    </h5>
-                    <p className="block font-sans text-xl font-normal leading-relaxed text-inherit antialiased">
-                        Rating: {rating} Brand: {brand_name}
+                    <div className="mb-2 flex items-center justify-between">
+                        <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                            {phone_name}
+                        </p>
+                        <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                            ${price}
+                        </p>
+                    </div>
+                    <p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+                        <> Brand: {brand_name} </> , <> Rating: {rating} </>
                     </p>
                 </div>
                 <div className="p-6 pt-0">
                     <button
-                        className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        className="block w-full select-none rounded-lg bg-purple-600 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button"
-                        data-ripple-light="true"
                     >
-                        Add Favourite
+                        Add to Favorite
                     </button>
                 </div>
             </div>
